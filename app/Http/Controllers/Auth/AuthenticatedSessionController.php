c7afd58eed45797fea7defcb06ed1582b64b4b60
ked-k <kedkayz@gmail.com>
2023-06-26 09:00:48 +0300

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -31,8 +31,12 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
-
-        return redirect()->intended(RouteServiceProvider::HOME);
+        if (Auth::user()->hasPermission(['view_reports'])){
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }else{
+            return to_route('audits');  
+        }
+        
     }
 
     /**
